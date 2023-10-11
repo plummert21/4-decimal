71680039f1d913b9c1754c07b98538988ce19500
Plummer Tweek <plummert@student.21-school.ru>
2023-10-11 21:03:42 +0500

    add function for long decimal

--- s21_func_bits.c
+++ s21_func_bits.c
@@ -6,9 +6,17 @@ int __get_bit_decimal(s21_decimal *num, int bit) {
   return __get_bit(num->bits[bit / 32], bit);
 }
 
+int __get_bit_long_decimal(s21_long_decimal *num, int bit) {
+  return __get_bit(num->bits[bit / 32], bit);
+}
+
 void __set_bit(int *num, int bit) { *num |= (1 << bit); }
 
 void __set_bit_decimal(s21_decimal *num, int bit) {
+  __set_bit(&num->bits[bit / 32], bit % 32);
+}
+
+void __set_bit_long_decimal(s21_long_decimal *num, int bit) {
   __set_bit(&num->bits[bit / 32], bit % 32);
 }
 
@@ -18,14 +26,36 @@ void __reset_bit_decimal(s21_decimal *num, int bit) {
   __reset_bit(&num->bits[bit / 32], bit % 32);
 }
 
+void __reset_bit_long_decimal(s21_long_decimal *num, int bit) {
+  __reset_bit(&num->bits[bit / 32], bit % 32);
+}
+
 void __print_bit(int num) {
-  for (int i = 31; i > -1; i--) printf("%d", __get_bit(num, i));
+  for (int i = 31; i >= 0; i--) printf("%d", __get_bit(num, i));
 }
 
 void __print_bit_decimal(s21_decimal *num) {
-  for (int i = 3; i > -1; i--) {
+  for (int i = 3; i >= 0; i--) {
     __print_bit(num->bits[i]);
     printf(" ");
   }
   printf("\n");
+}
+
+void __print_bit_long_decimal(s21_long_decimal *num) {
+  __print_bit(num->bits[6]);
+  printf("\n");
+  for (int i = 5; i >= 0; i--) {
+    __print_bit(num->bits[i]);
+    printf(" ");
+  }
+  printf("\n");
+}
+
+void __copy_decimal_to_long_decimal(s21_decimal *decimal,
+                                    s21_long_decimal *long_decimal) {
+  long_decimal->bits[6] = decimal->bits[3];
+  for (int i = 0; i < 3; i++) {
+    long_decimal->bits[i] = decimal->bits[i];
+  }
 }
--- s21_func_bits.h
+++ s21_func_bits.h
@@ -6,18 +6,27 @@
 #include "s21_decimal.h"
 
 // ToDo - Generic
-// ToDo - long decimal
 
 int __get_bit(int num, int bit);
 int __get_bit_decimal(s21_decimal *num, int bit);
+int __get_bit_long_decimal(s21_long_decimal *num, int bit);
 
 void __set_bit(int *num, int bit);
 void __set_bit_decimal(s21_decimal *num, int bit);
+void __set_bit_long_decimal(s21_long_decimal *num, int bit);
 
 void __reset_bit(int *num, int bit);
 void __reset_bit_decimal(s21_decimal *num, int bit);
+void __reset_bit_long_decimal(s21_long_decimal *num, int bit);
 
 void __print_bit(int num);
 void __print_bit_decimal(s21_decimal *num);
+void __print_bit_long_decimal(s21_long_decimal *num);
+
+// ToDo - __get_exp
+// int __get_exp(int )
+
+void __copy_decimal_to_long_decimal(s21_decimal *decimal,
+                                    s21_long_decimal *long_decimal);
 
 #endif
