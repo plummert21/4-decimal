50c35096aa67a0756beab815ade32eb9eafc8d57
Ваше Имя <you@example.com>
2023-10-11 13:12:13 +0500

    add type s21_long_decimal

--- s21_decimal.h
+++ s21_decimal.h
@@ -5,6 +5,10 @@ typedef struct {
   int bits[4];
 } s21_decimal;
 
+typedef struct {
+  unsigned int bits[7];
+} s21_long_decimal;
+
 // Арифметические операторы
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
