d063642726c7e39e7d6a52d3b8c045b3870f99c9
Ваше Имя <you@example.com>
2023-10-12 11:30:48 +0500

    change s21_is_less.c - compare sign and exp

--- s21_is_less.c
+++ s21_is_less.c
@@ -5,10 +5,23 @@ int s21_is_less(s21_decimal value_1, s21_decimal value_2) {
   int result = 2;
   int sign_value_1 = __get_bit_decimal(&value_1, sign_bit_decimal);
   int sign_value_2 = __get_bit_decimal(&value_2, sign_bit_decimal);
+  int exp_value_1 = __get_exp(value_1.bits[rank_exp_decimal]);
+  int exp_value_2 = __get_exp(value_2.bits[rank_exp_decimal]);
+  int diff_exp = exp_value_1 - exp_value_2;
   if (sign_value_1 == sign_value_2) {
-    // ToDo - перевод в long_decimal
-    // ToDo - нормализация long_decimal
-    // ToDo - сравнение модулей long_decimal
+    int count_rank = 3;
+    if (diff_exp) {
+      s21_long_decimal value_1_long;
+      s21_long_decimal value_2_long;
+      __copy_decimal_to_long_decimal(&value_1, &value_1_long);
+      __copy_decimal_to_long_decimal(&value_2, &value_2_long);
+      // ToDo - нормализация long_decimal
+      count_rank = 6;
+    } else {
+      // ToDo - сравнение модулей decimal or lond_decimal (в
+      // зависимости от значения count_rank).
+    }
+
   } else {
     result = sign_value_1 > sign_value_2 ? 1 : 0;
   }
