918a0f93ff0ebbd4142d1000ddd8298db61a3694
Ваше Имя <you@example.com>
2023-10-12 17:34:43 +0500

    add logic s21_is_equal

---
+++ s21_is_equal.c
@@ -0,0 +1,12 @@
+#include "s21_compare.h"
+#include "s21_decimal.h"
+#include "s21_func_bits.h"
+
+int s21_is_equal(s21_decimal value_1, s21_decimal value_2) {
+  int result = -1;
+  // Copy decimal to long decimal
+  // Normalize
+  // Compare long decimal
+  // if result compare = 7 -> equal.
+  return result;
+}
