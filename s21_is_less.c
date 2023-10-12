ec07c67d772a7fd6a9e9f8cdb8b010e4f45271b9
Ваше Имя <you@example.com>
2023-10-12 14:48:54 +0500

    change logic with different signs

--- s21_is_less.c
+++ s21_is_less.c
@@ -5,33 +5,19 @@
 int s21_is_less(s21_decimal value_1, s21_decimal value_2) {
   int result = -1;
   int compare = __compare_decimal(&value_1, &value_2);
-
+  if (compare == 7) {
+    result = 0;
+  } else if (!__get_bit(compare, compare_sign_bit)) {
+    // int sign_bit_value_1 = __get_bit_decimal(&value_1, sign_bit_decimal);
+    // int sign_bit_value_2 = __get_bit_decimal(&value_2, sign_bit_decimal);
+    // result = sign_bit_value_1 > sign_bit_value_2 ? 1 : 0;
+    result = __get_bit_decimal(&value_1, sign_bit_decimal) ? 1 : 0;
+  } else if (__get_bit(compare, compare_exp_bit)) {
+    // ToDo - знаки равны, экспоненты равны, сравниваем модули (не забыть про
+    // положительные и отрицаельные числа)
+  } else {
+    // ToDo - знаки равны, экспоненты не равны. Переводим в лонг-децимал,
+    // нормализуем, сравниваем модули лонг_децимал.
+  }
   return result;
 }
-
-// int s21_is_less(s21_decimal value_1, s21_decimal value_2) {
-//   int result = 2;
-//   int sign_value_1 = __get_bit_decimal(&value_1, sign_bit_decimal);
-//   int sign_value_2 = __get_bit_decimal(&value_2, sign_bit_decimal);
-//   int exp_value_1 = __get_exp(value_1.bits[rank_exp_decimal]);
-//   int exp_value_2 = __get_exp(value_2.bits[rank_exp_decimal]);
-//   int diff_exp = exp_value_1 - exp_value_2;
-//   if (sign_value_1 == sign_value_2) {
-//     int count_rank = 3;
-//     if (diff_exp) {
-//       s21_long_decimal value_1_long;
-//       s21_long_decimal value_2_long;
-//       __copy_decimal_to_long_decimal(&value_1, &value_1_long);
-//       __copy_decimal_to_long_decimal(&value_2, &value_2_long);
-//       // ToDo - нормализация long_decimal
-//       count_rank = 6;
-//     } else {
-//       // ToDo - сравнение модулей decimal or lond_decimal (в
-//       // зависимости от значения count_rank).
-//     }
-
-//   } else {
-//     result = sign_value_1 > sign_value_2 ? 1 : 0;
-//   }
-//   return result;
-// }
