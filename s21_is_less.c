bd4aa27bd724869fe6dfc479dcbf3f47259c0954
Ваше Имя <you@example.com>
2023-10-12 17:59:33 +0500

    change s21_is_less

--- s21_is_less.c
+++ s21_is_less.c
@@ -23,20 +23,12 @@ int s21_is_less(s21_decimal value_1, s21_decimal value_2) {
     result = bit_value_1 < bit_value_2 ? 1 : 0;
     if (__get_bit_decimal(&value_1, sign_bit_decimal)) result = !result;
   } else {
-    // ToDo - знаки равны, экспоненты не равны. Переводим в лонг-децимал,
-    // нормализуем, сравниваем модули лонг_децимал.
+    // ToDo - знаки равны, экспоненты не равны. Нормализуем с переводом в лонг
+    // децимал, сравниваем модули лонг_децимал.
     s21_long_decimal long_value_1 = {0};
     s21_long_decimal long_value_2 = {0};
-    int exp_value_1 = __get_exp(value_1.bits[rank_exp_decimal]);
-    int exp_value_2 = __get_exp(value_2.bits[rank_exp_decimal]);
-    __copy_decimal_to_long_decimal(&value_1, &long_value_1);
-    __copy_decimal_to_long_decimal(&value_2, &long_value_2);
-    int diff_exp = exp_value_1 - exp_value_2;
-    if (diff_exp < 0) {
-      // Нормализуем value_1
-    } else {
-      // Нормализуем value_2
-    }
+    __pre_compare_long_decimal(&value_1, &value_2, &long_value_1,
+                               &long_value_2);
   }
   return result;
 }
