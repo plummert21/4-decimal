753df76cd79aab1143087172fa19c56245c05868
Ваше Имя <you@example.com>
2023-10-12 13:20:20 +0500

    add __compare_sign, __compare_decimal

--- s21_compare.c
+++ s21_compare.c
@@ -6,7 +6,7 @@ int __compare_sign_decimal(s21_decimal *value_1, s21_decimal *value_2) {
   int sign_value_1 = __get_bit_decimal(value_1, sign_bit_decimal);
   int sign_value_2 = __get_bit_decimal(value_2, sign_bit_decimal);
   if (sign_value_1 == sign_value_2) {
-    result = 4;
+    __set_bit(&result, compare_sign_bit);
   }
   return result;
 }
@@ -21,10 +21,25 @@ int __compare_module_decimal(s21_decimal *value_1, s21_decimal *value_2) {
   return 0;
 }
 
+int __compare_decimal(s21_decimal *value_1, s21_decimal *value_2) {
+  int result = -1;
+  int compare_sign = __compare_sign_decimal(value_1, value_2);
+  int compare_exp = __compare_exp_decimal(value_1, value_2);
+  int compare_module = __compare_module_decimal(value_1, value_2);
+  int compare = compare_sign | compare_exp | compare_module;
+  return compare;
+}
+
 int __compare_sign_long_decimal(s21_long_decimal *value_1,
                                 s21_long_decimal *value_2) {
   // return 0 - false, 4 - true
-  return 0;
+  int result = 0;
+  int sign_value_1 = __get_bit_long_decimal(value_1, sign_bit_long_decimal);
+  int sign_value_2 = __get_bit_long_decimal(value_2, sign_bit_long_decimal);
+  if (sign_value_1 == sign_value_2) {
+    __set_bit(&result, compare_sign_bit);
+  }
+  return result;
 }
 
 int __compare_exp_long_decimal(s21_long_decimal *value_1,
--- s21_compare.h
+++ s21_compare.h
@@ -4,6 +4,10 @@
 #include "s21_decimal.h"
 #include "s21_func_bits.h"
 
+#define compare_sign_bit 2
+#define compare_exp_bit 1
+#define compare_module_bit 0
+
 // ToDo - Generic
 
 int __compare_sign_decimal(s21_decimal *value_1, s21_decimal *value_2);
