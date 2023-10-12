de1504c89ecd369ace6036ed13266a4976088000
Ваше Имя <you@example.com>
2023-10-12 14:44:17 +0500

    add compare decimal, compare long decimal

--- s21_compare.h
+++ s21_compare.h
@@ -10,10 +10,13 @@
 
 // ToDo - Generic
 
+int __compare_decimal(s21_decimal *value_1, s21_decimal *value_2);
 int __compare_sign_decimal(s21_decimal *value_1, s21_decimal *value_2);
 int __compare_exp_decimal(s21_decimal *value_1, s21_decimal *value_2);
 int __compare_module_decimal(s21_decimal *value_1, s21_decimal *value_2);
 
+int __compare_long_decimal(s21_long_decimal *value_1,
+                           s21_long_decimal *value_2);
 int __compare_sign_long_decimal(s21_long_decimal *value_1,
                                 s21_long_decimal *value_2);
 int __compare_exp_long_decimal(s21_long_decimal *value_1,
