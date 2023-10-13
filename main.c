dc83c0419bd46f931c961652de2bd13e4e04d224
Ваше Имя <you@example.com>
2023-10-13 15:57:39 +0500

    FINISH s21_is_greater_or_equal

--- main.c
+++ main.c
@@ -8,11 +8,11 @@
 int main() {
   s21_decimal num1 = {0};
   s21_decimal num2 = {0};
-  char str1[len_str_max] = "5.1";
+  char str1[len_str_max] = "5.00";
   char str2[len_str_max] = "5";
   s21_str_to_decimal(&num1, str1);
   s21_str_to_decimal(&num2, str2);
-  int greater = s21_is_greater(num1, num2);
-  printf("%d\n", greater);
+  int greater_or_equal = s21_is_greater_or_equal(num1, num2);
+  printf("%d\n", greater_or_equal);
   return 0;
 }
