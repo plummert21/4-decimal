600bbfb025bdcc14e367428519254336729866ae
Plummer Tweek <plummert@student.21-school.ru>
2023-10-15 19:54:36 +0500

    change s21_add

--- s21_add.c
+++ s21_add.c
@@ -10,16 +10,14 @@ int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
   __pre_compare_module_long_decimal(&value_1, &value_2, &long_value_1,
                                     &long_value_2);
   compare = __compare_long_decimal(&long_value_1, &long_value_2);
-  if (__get_bit(compare, 2)) {
+  if (__get_bit(compare, compare_sign_bit)) {
+    long_result.bits[rank_exp_long_decimal] =
+        long_value_1.bits[rank_exp_long_decimal];
     __add_module_long_decimal(long_value_1, long_value_2, &long_result);
-    result->bits[rank_exp_decimal] = value_1.bits[rank_exp_decimal];
-    // !!!!!!!!!!!!!!!!!!!!!!   ToDo   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    // round for big and small value
-    for (int i = 2; i >= 0; i--) {
-      result->bits[i] = long_result.bits[i];
-    }
   } else {
-    // ToDo разность если знаки различны
+    // ToDo разность если знаки различные
   }
+  // !!!!!!!!!!!!!!!!!!!!!!   ToDo   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  err = __long_decimal_to_decimal(&long_result, result);
   return err;
 }
