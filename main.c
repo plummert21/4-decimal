bf30b2722952cc9cf55368e40a3b192a2dc03956
Ваше Имя <you@example.com>
2023-10-19 16:24:34 +0500

    change main.c for tobiasar

--- main.c
+++ main.c
@@ -6,19 +6,22 @@
 #include "s21_func_str.h"
 
 int main() {
-  s21_decimal num1 = {0};
-  s21_decimal num2 = {0};
-  s21_decimal res = {0};
+  s21_decimal val = {0};
+  s21_long_decimal long_val = {0};
   int err = 0;
-  char str1[len_str_max] = "-5.89765958854";
-  char str2[len_str_max] = "-7.01";
-  char res_str[len_str_max] = {0};
-  s21_str_to_decimal(&num1, str1);
-  s21_str_to_decimal(&num2, str2);
-  err = s21_add(num1, num2, &res);
-  __print_bit_decimal(&res);
-  s21_decimal_to_str(&res, res_str);
-  s21_print_str(res_str);
-  printf("%d\n", err);
+  char str[len_str_max] = "7.1";
+  char long_str[len_str_max] = {0};
+
+  s21_str_to_decimal(&val, str);
+  s21_decimal_to_str(&val, str);
+  __print_bit_decimal(&val);
+  s21_print_str(str);
+
+  __print_bit_long_decimal(&long_val);
+  s21_str_to_long_decimal(&long_val, str);
+  s21_long_decimal_to_str(&long_val, str);
+  __print_bit_long_decimal(&long_val);
+  s21_print_str(str);
+
   return 0;
 }
